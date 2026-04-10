3d3f0ca3fef8a8f4a7b0cee9daa4125a883dd3c6
Ankit Parida <ankitparida386@gmail.com>
2026-04-10 11:42:50 +0530

    Adding the some changes

--- backend/routes/libraryRoutes.js
+++ backend/routes/libraryRoutes.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getLibrary, addToLibrary, removeFromLibrary } = require('../controllers/libraryController');
-
 router.get('/', getLibrary);
 router.post('/add', addToLibrary);
-
 router.delete('/remove/:id', removeFromLibrary); 
-
 module.exports = router;
